6b7725c33229a82d6f4ee9bc80d4478a78c89b13
chandru-chandrian <chandru9791865695@gmail.coom>
2025-06-06 13:38:51 +0530

    Fix ALLOWED_HOSTS and confirm settings for Render

--- Mainaapp/settings.py
+++ Mainaapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +29,7 @@ DEBUG = True
 
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','flameszone.onrender.com','127.0.0.1']
 
 print("ALLOWED_HOSTS =", ALLOWED_HOSTS) 
 
